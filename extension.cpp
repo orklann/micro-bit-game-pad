3542f493f71b06166908d5138a083f6a706f5afc
Aaron Elkins <threcius@yahoo.com>
2018-08-22 11:12:56 +0800

    After build fail

--- extension.cpp
+++ extension.cpp
@@ -1,9 +1,13 @@
 #include "pxt.h"
 using namespace pxt;
 
+/**
+ * A function to changer Microbit bluetooth device name 
+ */
+
 namespace bluetooth {
     //% blockId=bluetooth_setDeviceName block="bluetooth set device name %name"
     void setBluetoothDeviceName(char *name) {
-        uBit.ble.gap().setDeviceName((const uint8_t *)name);
+        (*uBit.ble).gap().setDeviceName((const uint8_t *)name);
     }
 }
--- shims.d.ts
+++ shims.d.ts
@@ -1,1 +1,11 @@
-// Will be auto-generated if needed.
+// Auto-generated. Do not edit.
+
+
+    /**
+     * A function to changer Microbit bluetooth device name 
+     */
+
+declare namespace bluetooth {
+}
+
+// Auto-generated. Do not edit. Really.
