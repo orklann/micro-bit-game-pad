c12ef62f499648153138d461a30fa9bb877bd31b
Aaron Elkins <threcius@yahoo.com>
2018-08-22 13:41:24 +0800

    Hacking

--- extension.cpp
+++ extension.cpp
@@ -4,10 +4,11 @@ namespace bluetooth {
     /**
      * A function to changer Microbit bluetooth device name
      */
-     
+
      //% blockId=bluetooth_setBLEDeviceName block="bluetooth set device name %name"
      void setBLEDeviceName(StringData *name) {
          ManagedString s(name);
-         (*uBit.ble).gap().setDeviceName((const uint8_t *)s.toCharArray());
+         //(*uBit.ble).gap().setDeviceName((const uint8_t *)s.toCharArray());
+         uBit.display.scroll(s, 1);
      }
 }
