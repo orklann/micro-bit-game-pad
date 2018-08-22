99cae058b50495f25445c355edb3c431c103eae1
Aaron Elkins <threcius@yahoo.com>
2018-08-22 13:52:19 +0800

    Final Boss

--- extension.cpp
+++ extension.cpp
@@ -11,8 +11,8 @@ namespace bluetooth {
      //% blockId=bluetooth_setBLEDeviceName block="bluetooth set device name %name"
      void setBLEDeviceName(StringData *name) {
          ManagedString s(name);
-         //(*uBit.ble).gap().setDeviceName((const uint8_t *)s.toCharArray());
-         uBit.display.scroll(s, 1);
+         (*uBit.ble).gap().setDeviceName((const uint8_t *)s.toCharArray());
+         //uBit.display.scroll(s, 1);
      }
 }
 
