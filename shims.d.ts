fa5779ff20301195af2f923567fdd4ec243e05f4
Aaron Elkins <threcius@yahoo.com>
2018-08-22 13:15:52 +0800

    Hacking

--- shims.d.ts
+++ shims.d.ts
@@ -1,8 +1,16 @@
 // Auto-generated. Do not edit.
+declare namespace bluetooth {
+
+    /**
+     * A function to changer Microbit bluetooth device name
+     */
+    //% blockId=bluetooth_setDeviceName_aaron_edition block="bluetooth set device name %name" shim=bluetooth::setDeviceName
+    function setDeviceName(name: string): void;
+}
 declare namespace basic {
 
     /**
-     * A function to show "Microbit"
+     * A function to show "Microbit
      */
     //% blockId=basic_showMyName block="basic show my name %name" shim=basic::showMyName
     function showMyName(name: string): void;
