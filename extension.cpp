11b2dbd0798367e7e20622f1109d2b6cd01c2a81
Aaron Elkins <threcius@yahoo.com>
2018-08-22 13:05:46 +0800

    Two function exported

--- extension.cpp
+++ extension.cpp
@@ -1,10 +1,25 @@
 #include "pxt.h"
+#include "MicroBitConfig.h"
+#include "ble/BLE.h"
 
 using namespace pxt;
 
+namespace bluetooth {
+    /**
+     * A function to changer Microbit bluetooth device name
+     */
+
+    //% blockId=bluetooth_setDeviceName block="bluetooth set device name %name"
+    void setDeviceName(StringData *name) {
+        ManagedString s(name);
+        (*uBit.ble).gap().setDeviceName((const uint8_t *)s.toCharArray());
+    }
+}
+
+
 namespace basic {
     /**
-     * A function to show "Microbit"
+     * A function to show "Microbit
      */
 
     //% blockId=basic_showMyName block="basic show my name %name"
