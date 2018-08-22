288d57dc5098ab9684c7acba8d39ba4e98b664c7
Aaron Elkins <threcius@yahoo.com>
2018-08-22 12:57:16 +0800

    Show my name

--- extension.cpp
+++ extension.cpp
@@ -15,3 +15,16 @@ namespace bluetooth {
         (*uBit.ble).gap().setDeviceName((const uint8_t *)s.toCharArray());
     }
 }
+
+
+namespace basic {
+    /**
+     * A function to show "Microbit"
+     */
+
+    //% blockId=basic_showMyName block="basic show my name %name"
+    void showMyName(StringData *name) {
+        ManagedString s(name);
+        uBit.display.scroll(s, 1);
+    }
+}
--- shims.d.ts
+++ shims.d.ts
@@ -7,5 +7,13 @@ declare namespace bluetooth {
     //% blockId=bluetooth_setDeviceName block="bluetooth set device name %name" shim=bluetooth::setDeviceName
     function setDeviceName(name: string): void;
 }
+declare namespace basic {
+
+    /**
+     * A function to show "Microbit"
+     */
+    //% blockId=basic_showMyName block="basic show my name %name" shim=basic::showMyName
+    function showMyName(name: string): void;
+}
 
 // Auto-generated. Do not edit. Really.
