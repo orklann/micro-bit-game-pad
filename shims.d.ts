ec7f613e9adc5e42ed555a24afda8bda4fade78d
Aaron Elkins <threcius@yahoo.com>
2018-08-22 11:42:03 +0800

    Edit shims.d.ts

--- shims.d.ts
+++ shims.d.ts
@@ -2,10 +2,12 @@
 
 
     /**
-     * A function to changer Microbit bluetooth device name 
+     * A function to changer Microbit bluetooth device name
      */
 
 declare namespace bluetooth {
+    //% blockId=bluetooth_setDeviceName block="bluetooth set device name %name" shim=bluetooth::setDeviceName
+    function setDeviceName(name: string): void;
 }
 
 // Auto-generated. Do not edit. Really.
