f4b1cf35a678b5167f2f473787bd7438df45beae
Aaron Elkins <threcius@yahoo.com>
2018-08-23 22:15:38 +0800

    Now the repo can be disovered by Mac and recognized as Gamepad by OpenEmu

--- test.ts
+++ test.ts
@@ -1,7 +1,7 @@
 // tests go here; this will not be compiled when this package is used as a library
 // tests go here; this will not be compiled when this package is used as a library
 input.onButtonPressed(Button.A, () => {
-    bluetooth.setJoystickButton(JoystickButton.JOYSTICK_BUTTON_1);
+    bluetooth.setJoystickButton(JoystickButton.JOYSTICK_BUTTON_1, 1);
 })
 basic.showString("OK")
 bluetooth.startJoystickService()
