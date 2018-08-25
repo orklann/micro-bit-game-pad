316bd4ee9348766bc6974fdb9f1304606d661551
Aaron Elkins <threcius@yahoo.com>
2018-08-25 13:30:35 +0800

    Remove sample projects

--- projects/Untitled/main.ts
+++ projects/Untitled/main.ts
@@ -1,28 +1,12 @@
-// tests go here; this will not be compiled when this package is used as a library
-input.onButtonPressed(Button.A, () => {
-    bluetooth.pressJoystickButton(JoystickButton.JOYSTICK_BUTTON_1);
-    basic.showString("A");
+input.onGesture(Gesture.ScreenUp, () => {
+    basic.showString("U")
 })
-
-// Two input work only with this pxt.json config
-// "yotta": {
-//     "config": {
-//         "microbit-dal": {
-//             "bluetooth": {
-//                 "dfu_service": 0,
-//                 "event_service": 0,
-//                 "device_info_service": 0,
-//                 "security_level": "SECURITY_MODE_ENCRYPTION_NO_MITM"
-//             },
-//             "gatt_table_size": "0x700"
-//         }
-//     }
-// }
-input.onButtonPressed(Button.B, () => {
-    bluetooth.pressJoystickButton(JoystickButton.JOYSTICK_BUTTON_2);
-    basic.showString("B");
+input.onGesture(Gesture.TiltLeft, () => {
+    basic.showString("SL")
 })
-
-
-basic.showString("OK")
-bluetooth.startJoystickService()
+input.onGesture(Gesture.ScreenDown, () => {
+    basic.showString("D")
+})
+input.onGesture(Gesture.TiltRight, () => {
+    basic.showString("SR")
+})
